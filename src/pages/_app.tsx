069f9b720444e55536c94c75aba6099a41d4e79b
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-03-11 17:54:31 +1100

    update: design system font & preload in next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Roboto } from "next/font/google";
+
+export const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
