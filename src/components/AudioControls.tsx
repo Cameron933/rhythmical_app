70604d9c626bebb485c237ecf5232c84147bf40b
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-03-21 20:00:21 +1100

    fix: button logic

--- src/components/AudioControls.tsx
+++ src/components/AudioControls.tsx
@@ -1,24 +1,31 @@
 import React from "react";
-import { PlayIcon, PauseIcon, ForwardIcon, BackwardIcon } from "@heroicons/react/24/solid";
+import {
+  PlayIcon,
+  PauseIcon,
+  ForwardIcon,
+  BackwardIcon,
+  ArrowPathIcon,
+  BarsArrowDownIcon,
+} from "@heroicons/react/24/solid";
 
 const AudioControls = ({ isPlaying, onPlayPauseClick, onPrevClick, onNextClick }: any) => {
   return (
-    <div className="w-screen flex items-center border justify-between mt-4">
-      <button type="button" className="w-6 h-6 text-primary" onClick={onPrevClick}>
+    <div className="flex items-center border justify-around mt-4">
+      <button type="button" className="w-12 h-12 text-primary" onClick={onPrevClick}>
         <BackwardIcon />
       </button>
 
       {isPlaying ? (
-        <button type="button" className="w-6 h-6 text-primary" onClick={onPlayPauseClick}>
-          <PlayIcon />
+        <button type="button" className="w-12 h-12 text-primary" onClick={onPlayPauseClick}>
+          <PauseIcon />
         </button>
       ) : (
-        <button type="button" className="w-6 h-6 text-primary" onClick={onPlayPauseClick}>
-          <PauseIcon />
+        <button type="button" className="w-12 h-12 text-primary" onClick={onPlayPauseClick}>
+          <PlayIcon />
         </button>
       )}
 
-      <button type="button" className="w-6 h-6 text-primary" onClick={onNextClick}>
+      <button type="button" className="w-12 h-12 text-primary" onClick={onNextClick}>
         <ForwardIcon />
       </button>
     </div>
--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -18,61 +18,81 @@ const Demo = (props: Props) => {
   const [currentMusic, setCurrentMusic] = useState<MusicDetail>(musicArray[0]);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
-  const audioPlayer = useRef<HTMLAudioElement | null>(null);
-  // const { duration }: any = audioPlayer.current?.currentTime;
-
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  // const intervalRef = useRef();
+  // const isReady = useRef<boolean>(false);
+  // const { duration }: any = audioRef.current?.currentTime;
   // const currentPercentage = duration ? `${(trackProgress / duration) * 100}%` : "0%";
   // const trackStyling = `
   //   -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(${currentPercentage}, #fff), color-stop(${currentPercentage}, #777))
   // `;
 
-  // console.log(audioPlayer.current?.volume);
+  // const startTimer = () => {
+  //   // Clear any timers already running
+  //   clearInterval(intervalRef.current);
+
+  //   intervalRef.current = setInterval(() => {
+  //     if (audioRef.current.ended) {
+  //       toNextTrack();
+  //     } else {
+  //       setTrackProgress(audioRef.current.currentTime);
+  //     }
+  //   }, [1000]);
+  // };
 
   const onPlayPauseClick = () => {
     setIsPlaying(!isPlaying);
-    isPlaying ? audioPlayer.current?.play() : audioPlayer.current?.pause();
+    isPlaying ? audioRef.current?.play() : audioRef.current?.pause();
   };
 
-  // const handleLastMusic = () => {
-  //   const currentMusicIndex = musicArray.findIndex((element) => element.url === currentMusic.url);
-  //   const lastMusicIndex = currentMusicIndex > 0 ? currentMusicIndex - 1 : musicArray.length - 1;
-  //   setCurrentMusic(musicArray[lastMusicIndex]);
-
-  //   setTimeout(() => {
-  //     audioPlayer.current?.play();
-  //   }, 500);
-  // };
   const toPrevMusic = () => {
-    const currentMusicIndex = musicArray.findIndex((element) => element.url === currentMusic.url);
-    const nextMusicIndex = currentMusicIndex < musicArray.length - 1 ? currentMusicIndex - 1 : 0;
+    const currentMusicIndex = musicArray.findIndex((music) => music.url === currentMusic.url);
+    const nextMusicIndex = currentMusicIndex - 1 > 0 ? currentMusicIndex - 1 : 0;
     setCurrentMusic(musicArray[nextMusicIndex]);
-
-    setTimeout(() => {
-      audioPlayer.current?.play();
-    }, 500);
+    setIsPlaying(true);
   };
 
   const toNextMusic = () => {
-    const currentMusicIndex = musicArray.findIndex((element) => element.url === currentMusic.url);
+    const currentMusicIndex = musicArray.findIndex((music) => music.url === currentMusic.url);
     const nextMusicIndex = currentMusicIndex < musicArray.length - 1 ? currentMusicIndex + 1 : 0;
     setCurrentMusic(musicArray[nextMusicIndex]);
-
-    setTimeout(() => {
-      audioPlayer.current?.play();
-    }, 500);
+    setIsPlaying(true);
   };
 
-  return (
-    <main>
-      <p className="self-center">I am music player</p>
-      <audio ref={audioPlayer} src={currentMusic.url} onEnded={toNextMusic} />
+  useEffect(() => {
+    if (isPlaying) {
+      setTimeout(() => {
+        audioRef.current?.play();
+      }, 300);
+    } else {
+      audioRef.current?.pause();
+    }
+  }, [isPlaying, currentMusic]);
 
-      <AudioControls
-        isPlaying={isPlaying}
-        onPlayPauseClick={onPlayPauseClick}
-        onPrevClick={toPrevMusic}
-        onNextClick={toNextMusic}
-      />
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <p className="text-h3_light">Music player</p>
+      <audio ref={audioRef} src={currentMusic.url} onEnded={toNextMusic} />
+      <div className="w-full">
+        <AudioControls
+          isPlaying={isPlaying}
+          onPlayPauseClick={onPlayPauseClick}
+          onPrevClick={toPrevMusic}
+          onNextClick={toNextMusic}
+        />
+        {/* <input
+          type="range"
+          value={trackProgress}
+          step="1"
+          min="0"
+          max={duration ? duration : `${duration}`}
+          className="progress"
+          onChange={(e) => onScrub(e.target.value)}
+          onMouseUp={onScrubEnd}
+          onKeyUp={onScrubEnd}
+          style={{ background: trackStyling }}
+        /> */}
+      </div>
     </main>
   );
 };
