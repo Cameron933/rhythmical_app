fa620f4bb87762e38b77eb8e98f5bf88517ad725
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-04-01 02:02:08 +1100

    feat/ finish sidebar link button in storybook

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -4,17 +4,17 @@ import { BsThreeDots, BsCollectionPlay } from "react-icons/bs";
 const SideBar = () => {
   return (
     <aside className="flex flex-col w-[320px] bg-black">
-      <div className="flex w-full h-28 justify-around items-center">
-        <div className="flex gap-2 items-center">
+      <div className="flex items-center justify-around w-full h-28">
+        <div className="flex items-center gap-2">
           <BsCollectionPlay className="w-8 h-8 text-light" />
           <h1 className="text-h2_regular text-light">Rhythmical</h1>
         </div>
-        <BsThreeDots className="w-6 h-6 text-light cursor cursor-pointer" />
+        <BsThreeDots className="w-6 h-6 cursor-pointer text-light cursor" />
       </div>
-      <div className="divider m-0" />
+      <div className="m-0 divider" />
       <div></div>
       <div className="relative">
-        <div className="absolute w-2 h-8 left-0 bg-primary" />
+        <div className="absolute left-0 w-2 h-8 bg-primary" />
         <div className="absolute w-16 h-8 left-2 bg-gradient-to-r from-primary/90 to-primary/0 " />
       </div>
     </aside>
