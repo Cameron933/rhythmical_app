c0754f1ab40f98709e89beab32883d9b62d079b0
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-04-01 02:26:19 +1100

    feat/ finish sidebar link button in storybook

--- src/layouts/SideBar.tsx
+++ src/layouts/SideBar.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { BsThreeDots, BsCollectionPlay } from "react-icons/bs";
+import { LinkButton } from "@/stories/LinkButton";
 
 const SideBar = () => {
   return (
-    <aside className="flex flex-col w-[320px] bg-black">
+    <aside className="flex flex-col w-[300px] bg-black">
       <div className="flex items-center justify-around w-full h-28">
         <div className="flex items-center gap-2">
           <BsCollectionPlay className="w-8 h-8 text-light" />
@@ -12,10 +13,27 @@ const SideBar = () => {
         <BsThreeDots className="w-6 h-6 cursor-pointer text-light cursor" />
       </div>
       <div className="m-0 divider" />
-      <div></div>
-      <div className="relative">
-        <div className="absolute left-0 w-2 h-8 bg-primary" />
-        <div className="absolute w-16 h-8 left-2 bg-gradient-to-r from-primary/90 to-primary/0 " />
+
+      <div className="flex flex-col gap-2">
+        <h1 className="mt-2 text-primary ml-7">MENU</h1>
+        <LinkButton iconName="RiHome5Fill">Home</LinkButton>
+        <LinkButton iconName="RiRocketFill">Discover</LinkButton>
+        <LinkButton iconName="RiAlbumFill">Albums</LinkButton>
+        <LinkButton iconName="RiUserStarFill">Artists</LinkButton>
+        <LinkButton iconName="RiMvFill">MV</LinkButton>
+      </div>
+      <div className="m-0 divider" />
+
+      <div className="flex flex-col gap-2">
+        <h1 className="mt-2 text-primary ml-7">YOUR LIBRARY</h1>
+        <LinkButton iconName="RiTimeFill">Recent Played</LinkButton>
+        <LinkButton iconName="RiHeart2Fill">Favorite Songs</LinkButton>
+      </div>
+      <div className="m-0 divider" />
+
+      <div className="flex flex-col gap-2">
+        <h1 className="mt-2 text-primary ml-7">PLAYLISTS</h1>
+        <LinkButton iconName="RiPlayListFill">Top Hit 2023 - USA</LinkButton>
       </div>
     </aside>
   );
