46fb0bdd69c337d189142464434743dc286792d6
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-04-18 23:40:07 +1000

    feat: time converter

---
+++ src/utils/timeConverter.ts
@@ -0,0 +1,12 @@
+export const timeConverter = (time: number | undefined) => {
+  if (!time) {
+    return "";
+  }
+
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+
+  const twoDigitMinutes = minutes.toString().padStart(2, "0");
+  const twoDigitSeconds = seconds.toString().padStart(2, "0");
+  return `${twoDigitMinutes}:${twoDigitSeconds}`;
+};
